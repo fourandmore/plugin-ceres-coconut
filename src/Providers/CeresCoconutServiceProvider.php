89b7e7dffe80b8a7ec0513a718f3008ca2d97921
fourandmore <steven.resch@four-and-more.net>
2022-05-20 17:09:15 +0200

    dsddd

--- src/Providers/CeresCoconutServiceProvider.php
+++ src/Providers/CeresCoconutServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace CeresCoconut\Providers;
 
-use IO\Extensions\Functions\Partial;
-use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\Templates\Twig;
+use IO\Helper\TemplateContainer;
+use IO\Extensions\Functions\Partial;
+use IO\Services\ItemSearch\Helper\ResultFieldTemplate;
+use Plenty\Plugin\ConfigRepository;
 
 class CeresCoconutServiceProvider extends ServiceProvider
 {
@@ -24,6 +27,7 @@ class CeresCoconutServiceProvider extends ServiceProvider
      */
     public function boot(Twig $twig, Dispatcher $eventDispatcher)
     {
+                $enabledOverrides = explode(", ", $config->get("CeresCoconut.templates.override"));
 
 $this->overrideTemplate("Ceres::PageDesign.Partials.Header.DefaultHeader.twig", "CeresCoconut::PageDesign.Partials.Header.DefaultHeader.twig");
 
