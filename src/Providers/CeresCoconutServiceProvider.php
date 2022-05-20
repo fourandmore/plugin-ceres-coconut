c3e26372b9cde43d145acff0949a96f12cbd1460
fourandmore <steven.resch@four-and-more.net>
2022-05-20 15:59:41 +0200

    test

--- src/Providers/CeresCoconutServiceProvider.php
+++ src/Providers/CeresCoconutServiceProvider.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Theme\Providers;
+namespace CeresCoconut\Providers;
 
 use IO\Extensions\Functions\Partial;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
 
-class ThemeServiceProvider extends ServiceProvider
+class CeresCoconutServiceProvider extends ServiceProvider
 {
 
     /**
