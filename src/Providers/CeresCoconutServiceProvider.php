57ccc4910ad1843c70aa079f33f3b0082c5058f6
fourandmore <steven.resch@four-and-more.net>
2022-05-23 10:00:50 +0200

    7

--- src/Providers/CeresCoconutServiceProvider.php
+++ src/Providers/CeresCoconutServiceProvider.php
@@ -3,31 +3,11 @@
 namespace CeresCoconut\Providers;
 
 use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\Events\Dispatcher;
-use Plenty\Plugin\Templates\Twig;
-use IO\Helper\TemplateContainer;
-use IO\Extensions\Functions\Partial;
-use IO\Services\ItemSearch\Helper\ResultFieldTemplate;
-use Plenty\Plugin\ConfigRepository;
-use Plenty\Modules\Webshop\CeresCoconut\Providers\CeresCoconutServiceProvider;
+use Plenty\Modules\Webshop\Template\Providers\TemplateServiceProvider;
 
+class CeresCoconutServiceProvider extends TemplateServiceProvider {	
 
-/**
- * Class CeresCoconutServiceProvider
- * @package CeresCoconut\Providers
- */
-class CeresCoconutServiceProvider extends ServiceProvider
-{
-    const PRIORITY = 0;
-
-    public function register()
-    {
-
-    }
-
-    public function boot(Twig $twig, Dispatcher $dispatcher, ConfigRepository $config)
-    {
-		   $this->overrideTemplate("Ceres::Category.Macros.CategoryTree.twig", "CeresCoconut::Category.Macros.CategoryTree.twig");
-	
-    }
+	public function boot() {
+		$this->overrideTemplate("Ceres::Category.Macros.CategoryTree", "CeresCoconut::Category.Macros.CategoryTree");
+	}
 }
